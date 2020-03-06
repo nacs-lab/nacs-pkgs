84ad7e3773efb9c0e6a10be6af3471de4904d8b5
Yichao Yu <yyc1992@gmail.com>
2020-03-06 12:00:40 -0500

    [linux-nacs-git] fix typo

--- arch-arm/linux-nacs-git/mkuramdisk.sh
+++ arch-arm/linux-nacs-git/mkuramdisk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkinitcpio -p linux$1
+mkinitcpio -p $1
 mkimage -A arm -O linux -T ramdisk -C gzip -d /boot/ramdisk.image.gz \
         /boot/uramdisk.image.gz
 rm /boot/ramdisk.image.gz
