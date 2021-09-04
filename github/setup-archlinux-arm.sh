2acd22ce7d49a1fb07a891e6705ebdd6341ac8f3
Yichao Yu <yyc1992@gmail.com>
2021-09-04 18:33:12 -0400

    [github/archlinux-arm] Add retry for downloading from ArchLinuxARM

--- github/setup-archlinux-arm.sh
+++ github/setup-archlinux-arm.sh
@@ -2,4 +2,7 @@
 
 pacman -S --noconfirm arch-arm-git armv7l-linux-gnueabihf-gcc
 mount binfmt_misc -t binfmt_misc /proc/sys/fs/binfmt_misc
-pacman-armv7 -Sy --noconfirm base-devel
+for ((i = 0; i < 10; i++)); do
+    # Retry a few times since the ArchLinux ARM mirrors seem to be unreliable at times.
+    pacman-armv7 -Sy --noconfirm base-devel && break
+done
