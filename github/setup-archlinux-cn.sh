9851ed388b30c565e397893551467fc58359b17e
Yichao Yu <yyc1992@gmail.com>
2024-03-17 13:58:55 -0400

    fix archlinuxcn setup

--- github/setup-archlinux-cn.sh
+++ github/setup-archlinux-cn.sh
@@ -5,5 +5,9 @@ cat >> /etc/pacman.conf <<EOF
 Server = https://repo.archlinuxcn.org/\$arch
 EOF
 
+# https://github.com/archlinuxcn/repo/issues/3557
+# https://www.archlinuxcn.org/archlinuxcn-keyring-manually-trust-farseerfc-key/
+pacman-key --lsign-key "farseerfc@archlinux.org"
+
 pacman -Sy --noconfirm archlinuxcn-keyring
 pacman -Sy --noconfirm devtools-cn
